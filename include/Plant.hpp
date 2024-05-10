ba81fdb82870700366bb5ba395cc572540f6da16
Nafiseh Moghanizadeh <nmoghanizadeh@gmail.com>
2024-05-10 03:26:24 -0700

    add common plants featurs

--- include/Plant.hpp
+++ include/Plant.hpp
@@ -4,23 +4,31 @@
 #include <algorithm>
 #include <map>
 #include <list>
+#include <string>
 #include "Defs.hpp"
 
 class Plant
 {
     public:
     Plant(){};
-    Plant(map<string, int> config);
+    Plant(map<string, int> config,string plant_tex_file,const FloatRect bg_bound);
     void render(RenderWindow& window);
     void update();
 
-    private:
-    damage;
-    health;
-    hit_rate;
-    speed;
-    cool_down;
-    price;
+    protected:
+    Sprite plant_sp;
+    Sprite deck_sp;
+    Texture plant_tex;
+    Font font;
+    Text deck_txt;
+    int damage;
+    int health;
+    int hit_rate;
+    int speed;
+    int cool_down;
+    int price;
+    Clock clock;
+    
 };
 
 #endif
--- src/Plant.cpp
+++ src/Plant.cpp
@@ -1,1 +1,36 @@
-#include "plant.hpp"
+#include "Plant.hpp"
+
+Plant::Plant(map<string, int> config,string plant_tex_file,const FloatRect bg_bound)
+{
+    this-> damage = config['damage'];
+    this-> health = config['health'];
+    this-> hit_rate = config['hit_rate'];
+    this-> speed = config['speed'];
+    this-> cool_down = config['cool_down'];
+    this-> price = config['price'];
+    if (!plant_tex.loadFromFile(IMAGES_PATH + string plant_tex_file)) 
+    {
+        cerr << FILE_FAILED_MESSAGE << endl;
+    }
+    if (!font.loadFromFile(FONTS_PATH + "score.ttf")) 
+    {
+        cerr << FILE_FAILED_MESSAGE << endl;
+    }
+
+    IntRect subrect(0, 0, 350, 325);
+    deck_sp.setTexture(plant_tex);
+    deck_sp.setTextureRect(subrect);
+    deck_sp.setPosition(bg_bound.left+70, bg_bound.top+30);//later change
+    deck_sp.setScale(0.25, 0.25);
+    deck_txt.setFont(font);
+
+    plant_sp.setTexture(plant_tex);
+    plant_sp.setTextureRect(subrect);
+    plant_sp.setScale(0.25, 0.25);
+
+    //deck_txt.setString(to_string(sun_budget));
+    deck_txt.setFillColor(Color::Black);
+    deck_txt.setPosition(Vector2f(bg_bound.left+170, bg_bound.top+50));//later change
+    clock.restart();
+
+}
