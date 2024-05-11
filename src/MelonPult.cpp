52346d0f83f1709fcc91d03d33b9b0d7b3e50a48
Fardin Abbasi <fardinabbasi1381@gmail.com>
2024-05-11 22:20:30 +0330

    write toplevel functionality

--- src/MelonPult.cpp
+++ src/MelonPult.cpp
@@ -5,5 +5,15 @@
 void MelonPult::update(){
     if(clock.getElapsedTime().asSeconds() >= config["HitRate"]){
         melons.push_back(melon);
+        clock.restart();
+    }
+    auto melon_it = melons.begin();
+    while(it != melons.end()){
+        it->move(Vector2f(config["Speed"], ?));
+        if(?){//if arrived
+            if(zombie->kill(config["Damage"])){
+                zombie = nullptr;
+            }
+        }
     }
 }
