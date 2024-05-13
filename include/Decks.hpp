09e4ba1d141454230e5c28f643237ac9cad784e0
Nafiseh Moghanizadeh <nmoghanizadeh@gmail.com>
2024-05-13 08:56:15 -0700

    add decks file

---
+++ include/Decks.hpp
@@ -0,0 +1,14 @@
+#ifndef Decks_HPP
+#define Decks_HPP
+
+#include <string>
+#include <map>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+#endif
